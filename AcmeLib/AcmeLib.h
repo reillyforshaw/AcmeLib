5ae77e8af38aaf1f3900d0536ca820c5ff707521
Reilly Forshaw <reilly.forshaw@gmail.com>
2018-03-04 16:28:13 -0800

    Initial Commit

---
+++ AcmeLib/AcmeLib.h
@@ -0,0 +1,19 @@
+//
+//  AcmeLib.h
+//  AcmeLib
+//
+//  Created by Reilly Forshaw on 2018-03-04.
+//  Copyright © 2018 Reilly Forshaw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AcmeLib.
+FOUNDATION_EXPORT double AcmeLibVersionNumber;
+
+//! Project version string for AcmeLib.
+FOUNDATION_EXPORT const unsigned char AcmeLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AcmeLib/PublicHeader.h>
+
+
